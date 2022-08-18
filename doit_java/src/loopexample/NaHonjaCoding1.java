ca85baeeccb7e44c42100b7c1c162fd55489bc4b
LimNakHyun <dlaskrgus8@gmail.com>
2022-08-18 09:08:42 +0900

    08 / 17 (Chap02 ~ Chap04)

--- doit_java/src/loopexample/NaHonjaCoding1.java
+++ doit_java/src/loopexample/NaHonjaCoding1.java
@@ -1,6 +1,6 @@
 package loopexample;
 
-public class NaHonjaCoding3 {
+public class NaHonjaCoding1 {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 10; i++) {
 			System.out.println("안녕하세요"+i);
--- doit_java/src/loopexample/NaHonjaCoding2.java
+++ doit_java/src/loopexample/NaHonjaCoding2.java
@@ -1,6 +1,6 @@
 package loopexample;
 
-public class NaHonjaCoding4 {
+public class NaHonjaCoding2 {
 	public static void main(String[] args) {
 		int sum = 0;
 		int num;
