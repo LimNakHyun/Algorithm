48c8e21e7b0b02540670818429252d1665a7e5bd
LimNakHyun <dlaskrgus8@gmail.com>
2022-10-24 22:29:59 +0900

    test

---
+++ Programmers/src/problem/lv0/나머지_구하기/Main.java
@@ -0,0 +1,19 @@
+package problem.lv0.나머지_구하기;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
+
+public class Main {
+	public static void main(String[] args) {
+
+		Solution sol = new Solution();
+		int ans = sol.solution(3, 2);
+		
+		System.out.println("답 : " + ans);
+		
+	}
+	
+}
