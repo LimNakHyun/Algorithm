43825014e8fd256b86eebdd90785129b9685f636
LimNakHyun <dlaskrgus8@gmail.com>
2022-10-30 23:54:24 +0900

    사칙연산자 "*"

--- Programmers/src/problem/lv0/두_수의_곱/Main.java
+++ Programmers/src/problem/lv0/두_수의_곱/Main.java
@@ -10,7 +10,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		Solution sol = new Solution();
-		int ans = sol.solution(3, 4);
+		//좌측의 수와 우측의 수를 곱한 값을 리턴
+		int ans = sol.solution(3, 4);	//answer = 12
 		
 		System.out.println("답 : " + ans);
 		
