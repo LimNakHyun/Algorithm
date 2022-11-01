5e01223344c1d2a32f5c91e3559dd87198ddc4b4
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 15:50:05 +0900

    배열을 순회하며, 카운팅

--- Programmers/src/problem/lv0/중복된_숫자_개수/Main.java
+++ Programmers/src/problem/lv0/중복된_숫자_개수/Main.java
@@ -15,9 +15,11 @@ public class Main {
 		
 		int[] array = {1, 1, 2, 3, 4, 5};
 		int n = 1;
+        //배열 array와 숫자 n이 주어졌을 때,
+        //array 안에 n이 몇개 있는지 리턴
 
 		Solution sol = new Solution();
-		int ans = sol.solution(array, n);
+		int ans = sol.solution(array, n);   //array 안에 n값인 1이 2개 있기 때문에 2를 리턴
 		
 		System.out.println("답 : " + ans);
 		
