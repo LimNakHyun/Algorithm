856c82f1c938b25d04b2eabaafed113ff30915a4
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 17:49:27 +0900

    등차수열의 합 공식

--- Programmers/src/problem/lv0/짝수의_합/Main.java
+++ Programmers/src/problem/lv0/짝수의_합/Main.java
@@ -10,7 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 
 		Solution sol = new Solution();
+		//자연수 n이 주어졌을 때, n까지의 짝수의 합을 리턴
+
 		int ans = sol.solution(10);
+		//n = 10 이므로 2, 4, 6, 8, 10 의 합인 30을 리턴
 		
 		System.out.println("답 : " + ans);
 		
