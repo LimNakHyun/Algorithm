07d62ec80b8fd7541beb1e1745b34cca72ab12af
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 11:37:11 +0900

    배열을 순회하며 평균 구하기

--- Programmers/src/problem/lv0/배열의_평균값/Main.java
+++ Programmers/src/problem/lv0/배열의_평균값/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int[] numbers = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+		//numbers 배열이 주어지면 배열을 순회하며 숫자를 더한 후 평균을 구한다.
 
 		Solution sol = new Solution();
 		double ans = sol.solution(numbers);
