d7ba75fc22e0528b1cdc697552a5d6326e089ffb
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 15:47:42 +0900

    자릿수 합 알고리즘

--- Programmers/src/problem/lv0/자릿수_더하기/Main.java
+++ Programmers/src/problem/lv0/자릿수_더하기/Main.java
@@ -16,9 +16,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int n = 930211;
+        //숫자 n이 주어졌을 때, 그 자릿수의 합을 리턴
 
 		Solution sol = new Solution();
-		int ans = sol.solution(n);
+		int ans = sol.solution(n);  //여기서는 n = 930211 이므로, 16을 리턴함
 		
 		System.out.println("답 : " + ans);
 		
