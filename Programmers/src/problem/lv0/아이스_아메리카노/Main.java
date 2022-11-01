81ed8fb5e18922035908cc1fe311778f0fdd65d5
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 15:42:03 +0900

    기본적인 배열 다루기

--- Programmers/src/problem/lv0/아이스_아메리카노/Main.java
+++ Programmers/src/problem/lv0/아이스_아메리카노/Main.java
@@ -12,6 +12,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int money = 15000;
+		//money가 주어졌을 때, 5500원짜리 커피를 몇 잔 살수있는지, 
+		//그리고 남은 거스름돈은 얼마인지 각각의 값을 배열에다 리턴하기
 
 		Solution sol = new Solution();
 		int[] ans = sol.solution(money);
