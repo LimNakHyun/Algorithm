bf58153984e2733b96ddb243283afd4af6a90ce4
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 17:51:13 +0900

    문자열 길이 메소드

--- Programmers/src/problem/lv0/편지/Main.java
+++ Programmers/src/problem/lv0/편지/Main.java
@@ -10,9 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		String message = "happy birthday!";
+		//message 문자열이 주어지면 문자열 길이의 2배를 리턴
 
 		Solution sol = new Solution();
-		int ans = sol.solution(message);
+		int ans = sol.solution(message);	//message의 길이가 15이므로 2배인 30을 리턴
 		
 		System.out.println("답 : " + ans);
 		
