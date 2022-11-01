4a34be78f103f2f3afb37d5590507d95fb9290b0
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-01 17:57:50 +0900

    3진법 변환 메소드

--- Programmers/src/problem/lv1/삼진법_뒤집기/Main.java
+++ Programmers/src/problem/lv1/삼진법_뒤집기/Main.java
@@ -13,22 +13,22 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-//		int n = 45;		//7
-		int n = 125;	//229
-//		System.out.println((int)'0');
+		// int n = 45;		//45 -> 1200 -> 0021 -> 7
+		int n = 125;	//125 -> 11122 -> 22111 -> 229
+
+		//자연수 n 이 주어지면 그 수를 3진법으로 바꾼 후 뒤집은 것을 10진법으로 리턴
 		
 		StringBuilder sb = new StringBuilder();
 		while(n > 0) {
 			sb.append(n % 3);
 			n /= 3;
-//			System.out.println("저장된 숫자 : " + sb.toString());
-//			System.out.println("n 값 : " + n);
-//			System.out.println();
+			// System.out.println("저장된 숫자 : " + sb.toString());
+			// System.out.println("n 값 : " + n);
+			// System.out.println();
 		}
 		
-//		System.out.println("뒤집은 숫자 : " + sb.reverse());
-//		
-//		System.out.println("답 : " + numToTernary(sb));
+		// System.out.println("뒤집은 숫자 : " + sb.reverse());
+		// System.out.println("답 : " + numToTernary(sb));
 		
 		System.out.println("답 : " + Integer.parseInt(sb.toString(), 3));
 		
