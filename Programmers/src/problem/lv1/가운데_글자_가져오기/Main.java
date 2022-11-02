b5d609d0386e46fb9f7d6f04720b53f37f0177ff
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-02 21:54:00 +0900

    substring 메소드로 가운데 글자 찾기

---
+++ Programmers/src/problem/lv1/가운데_글자_가져오기/Main.java
@@ -0,0 +1,21 @@
+package problem.lv1.가운데_글자_가져오기;
+
+public class Main {
+
+    static String solution(String s) {
+        int len = s.length();
+        if(len % 2 == 0) {
+            return s.substring(len/2 - 1, len/2 + 1);
+        } else{
+            return s.substring(len/2, len/2 + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 문자열 s가 주어졌을 때, 그 문자열의 가운데 문자를 리턴
+
+        String s = "abcde";
+        System.out.println("가운데 글자 : " + solution(s));
+    }
+
+}
