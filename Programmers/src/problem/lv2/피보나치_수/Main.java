b525cff1b034a6f9e1092ced67127573b8f0cb43
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-03 10:07:08 +0900

    기초적인 메모이제이션을 활용한 다이나믹 프로그래밍

--- Programmers/src/problem/lv2/피보나치_수/Main.java
+++ Programmers/src/problem/lv2/피보나치_수/Main.java
@@ -5,9 +5,11 @@ import library.numberTheory.Fibonacci;
 
 public class Main {
 	
-	
 	public static void main(String[] args) {
-		
+		//자연수 n이 주어졌을 때,
+		//n번째 피보나치 숫자를 1234567로 나눈 값을 리턴
+
+		int n = 10000;
 //		for(int i = 0; i < 31; i++) {
 //			System.out.println((i + 1) + "번째 피보나치 숫자 : " + reCursiveFibo(i));
 //			System.out.println();
@@ -16,7 +18,7 @@ public class Main {
 //		System.out.println(variableFibo(80));
 //		System.out.println(Fibonacci.variableFibo(80) == 23416728348467685L);
 //		System.out.println(variableFibo(100) == 354224848179261915075L);
-		System.out.println(Fibonacci.dynamicFibo(10000));
+		System.out.println("(10000번째 피보나치 수) % 1234567 = " + Fibonacci.dynamicFibo(n));
 	}
 	
 }
