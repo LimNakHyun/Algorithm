c31fd4e5883773c63a8bae9545855e69af6b779d
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-09 20:18:19 +0900

    배열 원소 스위칭

---
+++ Programmers/src/library/baseConcept/Swap.java
@@ -0,0 +1,11 @@
+package library.baseConcept;
+
+public class Swap {
+	// 배열 arr 과 두 정수가 주어졌을 때,
+	// 배열에서 주어진 두 정수의 인덱스 요소
+    public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
