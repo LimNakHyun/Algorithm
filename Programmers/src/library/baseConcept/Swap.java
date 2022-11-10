1d9c3e5b7b6ec2c473af6f8f1c5ac9f7547bf05d
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-11 00:04:27 +0900

    char 배열 자리변경 추가

--- Programmers/src/library/baseConcept/Swap.java
+++ Programmers/src/library/baseConcept/Swap.java
@@ -3,8 +3,14 @@ package library.baseConcept;
 public class Swap {
 	// 배열 arr 과 두 정수가 주어졌을 때,
 	// 배열에서 주어진 두 정수의 인덱스 요소
-    public static void swap(int[] arr, int i, int j) {
+    public static void swapInt(int[] arr, int i, int j) {
 		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public static void swapChar(char[] arr, int i, int j) {
+		char temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
