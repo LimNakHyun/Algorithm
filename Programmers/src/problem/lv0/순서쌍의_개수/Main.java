b61625c47733ad06d39445bbec139aa9510cf1e7
LimNakHyun <dlaskrgus8@gmail.com>
2022-11-12 17:48:53 +0900

    sqrt 메소드를 활용한 약수의 개수 찾기

--- Programmers/src/problem/lv0/순서쌍의_개수/Main.java
+++ Programmers/src/problem/lv0/순서쌍의_개수/Main.java
@@ -14,8 +14,8 @@ public class Main {
         //순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기한다.
         //자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 리턴
 
-        // int n = 20;      //6을 리턴
-        int n = 100;        //9를 리턴
+        // int n = 20;      //answer = 6
+        int n = 100;        //answer = 9
 
         System.out.println("순서쌍의 개수: " + solution(n));
     }
