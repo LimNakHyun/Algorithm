c4f4636be965d9cb867911e29ab0f05d06d9b9db
LimNakHyun <dlaskrgus8@gmail.com>
2022-12-06 04:19:44 +0900

    char 배열도 정렬할 수 있게 업데이트

--- Programmers/src/library/sortingAlgorithm/BubbleSort.java
+++ Programmers/src/library/sortingAlgorithm/BubbleSort.java
@@ -5,7 +5,7 @@ import library.baseConcept.Swap;
 public class BubbleSort {
 	//시간복잡도가 O(n^2) 인 정렬 버블솔트
 
-	public static void bubbleSort(int[] arr, boolean rev) {
+	public static void intBubbleSort(int[] arr, boolean rev) {
 		for(int i = 0; i < arr.length - 1; i++) {
 			for(int j = i + 1; j < arr.length; j++) {
 				if(arr[i] < arr[j] && rev) {
@@ -13,6 +13,19 @@ public class BubbleSort {
 				}
 				if(arr[i] > arr[j] && !rev) {
 					Swap.swapInt(arr, i, j);
+				}
+			}
+		}
+	}
+
+	public static void charBubbleSort(char[] arr, boolean rev) {
+		for(int i = 0; i < arr.length - 1; i++) {
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[i] < arr[j] && rev) {
+					Swap.swapChar(arr, i, j);
+				}
+				if(arr[i] > arr[j] && !rev) {
+					Swap.swapChar(arr, i, j);
 				}
 			}
 		}
